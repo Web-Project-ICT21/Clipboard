1184fe1295cf82f099c02675f75d9fbc8b918afa
Dev Patel <devrpatel2804@gmail.com>
2024-04-23 21:28:07 +0530

    script.js

--- script.js
+++ script.js
@@ -34,3 +34,38 @@ function saveToClipboard() {
     document.getElementById("clipname1").value = "";
     document.getElementById("clipboard").value = "";
 }
+function loadFromClipboard() {
+    let clipName = document.getElementById("clipname").value;
+    let clipTextarea = document.getElementById("clipboard");
+
+    if (clipName in savedClips) {
+        clipTextarea.value = savedClips[clipName].text;
+        clipTextarea.focus(); // Set focus to the textarea
+    } else {
+        alert("Clip not found.");
+    }
+}
+
+function deleteClip() {
+    let clipName = document.getElementById("clipname").value;
+
+    if (clipName in savedClips) {
+        delete savedClips[clipName];
+        localStorage.setItem('savedClips', JSON.stringify(savedClips));
+        alert('Clip deleted successfully!');
+    } else {
+        alert("Clip not found.");
+    }
+    document.getElementById("clipname").value = "";
+    document.getElementById("clipboard").value = "";
+}
+
+function copyToClipboard() {
+    let clipText = document.getElementById("clipboard").value;
+
+    // Create a temporary textarea element
+    let tempTextarea = document.createElement("textarea");
+    tempTextarea.value = clipText;
+
+    // Append the textarea to the body
+    document.body.appendChild(tempTextarea);
